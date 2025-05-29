5ed0c04a4d3e87644539ab9f4518ace453890cfe
Rafky Ferdian Algiffari <117822812+rafenri22@users.noreply.github.com>
2025-05-29 15:08:34 +0700

    Update dbaplikasigajikaryawan.sql

--- dbaplikasigajikaryawan.sql
+++ dbaplikasigajikaryawan.sql
@@ -36,19 +36,6 @@ CREATE TABLE `tbgaji` (
   `tunjangan` decimal(12,2) DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbgaji`
---
-
-INSERT INTO `tbgaji` (`id`, `ktp`, `kodepekerjaan`, `gajibersih`, `gajikotor`, `tunjangan`) VALUES
-(20, '221011401796', 'RFA001', 5000000.00, 500000.00, 250000.00),
-(21, '221011401797', 'RFA001', 5000000.00, 500000.00, 250000.00),
-(22, '221011401798', 'RFA001', 5000000.00, 500000.00, 250000.00),
-(23, '221011401799', 'RFA002', 5000000.00, 250000.00, 600000.00),
-(24, '221011401795', 'RFA003', 5000000.00, 500000.00, 100000.00),
-(25, '221011401791', 'RFA004', 6000000.00, 100000.00, 250000.00),
-(27, '221011401777', 'RFA004', 10000.00, 10000.00, 0.00);
-
 -- --------------------------------------------------------
 
 --
@@ -62,19 +49,6 @@ CREATE TABLE `tbkaryawan` (
   `password` varchar(100) NOT NULL,
   `kode_pekerjaan` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tbkaryawan`
---
-
-INSERT INTO `tbkaryawan` (`ktp`, `nama`, `ruang`, `password`, `kode_pekerjaan`) VALUES
-('221011401777', 'Mbappe', 3, '9720f047b7055320d6df8fc33972bc2d', 'RFA004'),
-('221011401791', 'Messi', 5, '9720f047b7055320d6df8fc33972bc2d', 'RFA004'),
-('221011401795', 'Ronaldo', 3, '9720f047b7055320d6df8fc33972bc2d', 'RFA003'),
-('221011401796', 'Rafky', 1, '9720f047b7055320d6df8fc33972bc2d', 'RFA001'),
-('221011401797', 'Ferdian', 1, '9720f047b7055320d6df8fc33972bc2d', 'RFA001'),
-('221011401798', 'Algiffari', 1, '9720f047b7055320d6df8fc33972bc2d', 'RFA001'),
-('221011401799', 'Cristiano', 2, '9720f047b7055320d6df8fc33972bc2d', 'RFA002');
 
 -- --------------------------------------------------------
 
